ae92f932b45819eadde5e82306ba2e97a41dcccf
Max Gonzih <gonzih@gmail.com>
2012-08-31 19:37:26 +0300

    update description and url in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject irr_by_notify "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Simple Clojure bot for irr.by ads."
+  :url "https://github.com/Gonzih/simple-irr-by-bot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
