2f127c3113d5d29e4a140b5ea494b665ec3e0139
Max Gonzih <gonzih@gmail.com>
2012-09-05 10:17:36 +0300

    catch UnknownHostException

--- src/simple_bot/core.clj
+++ src/simple_bot/core.clj
@@ -1,12 +1,16 @@
 (ns simple-bot.core
   (:use     [clojure.tools.logging :only (info error)])
-  (:require [simple-bot.memoization :as memo]))
+  (:require [simple-bot.memoization :as memo])
+  (:import  [java.net UnknownHostException]))
 
 (def sleep (* 1000 60 10))
 
 (defn -main [& args]
   (loop []
-    (memo/check-for-new)
+    (try
+      (memo/check-for-new)
+      (catch UnknownHostException e
+        (info "Catched UnknownHostException for" (.getMessage e))))
     (info "Waiting...")
     (Thread/sleep sleep)
     (recur)))
