5db313a4efa965138a389823a1ccaffb656e1683
Max Gonzih <gonzih@gmail.com>
2012-09-06 11:27:33 +0300

    use set for storing old items, send notifications for all items on startup

--- src/simple_bot/memoization.clj
+++ src/simple_bot/memoization.clj
@@ -3,33 +3,19 @@
   (:require [simple-bot.fetch :as fetch]
             [simple-bot.notify :as notify]))
 
-(def last-item (atom nil))
+(def old-items (atom #{}))
 
-(defn update-last [item]
-  (reset! last-item (:hash item)))
+(defn add-to-old [items]
+  (doall (map #(swap! old-items conj (:hash %)) items)))
 
-(defn get-new
-  ([data]
-    (get-new data []))
-  ([data coll]
-    (let [item (first data)]
-      (if (= @last-item (:hash item))
-        coll
-        (let [result (conj coll item)]
-          (recur (rest data) result))))))
+(defn get-new [data]
+  (filter #(not (contains? @old-items (:hash %))) data))
 
 (defn check-for-new []
   (info "Checking...")
-  (if (nil? @last-item)
-    (let [item (first (fetch/results))]
-      (info "last-item is nil")
-      (update-last item)
-      (notify/items [item]))
-    (let [data (fetch/results)
-          new-items (get-new data)
-          new-item (first new-items)]
-      (info "last-item is not nil")
-      (when (> (count new-items) 0)
-        (info "Found " (count new-items) " new items")
-        (update-last new-item)
-        (notify/items new-items)))))
+  (let [data (fetch/results)
+        new-items (get-new data)]
+    (info "Found " (count new-items) " new items")
+    (add-to-old new-items)
+    (when (> (count new-items) 0)
+      (notify/items new-items))))
