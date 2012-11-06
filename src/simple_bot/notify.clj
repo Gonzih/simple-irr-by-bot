3ed2693a9e292485b2a1e451ccb328385e0bc7ef
Max Gonzih <gonzih@gmail.com>
2012-11-06 14:07:40 +0300

    fetch to emails from env

--- src/simple_bot/notify.clj
+++ src/simple_bot/notify.clj
@@ -3,14 +3,18 @@
         [clojure.tools.logging :only (info error)])
   (:import org.apache.commons.mail.HtmlEmail))
 
+(def target-emails []
+  "Emails should be in EMAILS env var, like 'one@email.com;second@email.com'"
+  (-> (System/getenv) (get "EMAILS") (clojure.string/split #";")))
 
 (defn setup-and-send-email [^HtmlEmail mail body]
+  (doseq [email (target-emails)]
+    (.addTo mail email))
   (doto mail
         (.setHostName "smtp.gmail.com")
         (.setSslSmtpPort "465")
         (.setSSL true)
         (.setTLS true)
-        (.addTo "gonzih@gmail.com")
         (.setFrom "bot@gonzih.org" "gnzh's bot")
         (.setSubject "New ad on irr.by")
         (.setCharset "UTF-8")
